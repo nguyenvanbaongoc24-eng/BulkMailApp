6416d8c75142352aa33d3b537a363b7266b21f71
nguyenvanbaongoc24-eng <nguyenvan.baongoc24@gmail.com>
2026-05-19 13:43:30 +0700

    diag: add commit hash to diag route

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.get('/api/diag', async (req, res) => {
         
         res.json({
             nodeVersion: process.version,
+            diagCommit: "c14092e",
             heartbeat: hb,
             hasServiceKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
             hasSmtpUser: !!process.env.SMTP_USER,
